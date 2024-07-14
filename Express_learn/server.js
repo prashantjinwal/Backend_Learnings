e00cf9b8d22a052f3c2ec88a44466c01ffc7aa1c
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-07-14 21:01:06 +0530

    add ejs

--- Express_learn/server.js
+++ Express_learn/server.js
@@ -2,7 +2,9 @@ const express = require("express")
 const app = express()
 
 app.get( "/" ,(req, res) => {
-     res.sendStatus(500)
+    //  res.sendStatus(500)
+    //  res.status(500).send("hi")
+    //  res.download("server.js")
 })
 
 app.listen(3000)
