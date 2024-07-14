06c3ac9d67eeeb27ea188c5c55d5596e86e74dc7
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-07-14 21:12:20 +0530

    router

--- Express_learn/server.js
+++ Express_learn/server.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const app = express()
 
+app.set("view engine", "ejs")
+
 app.get( "/" ,(req, res) => {
     //  res.sendStatus(500)
     //  res.status(500).send("hi")
     //  res.download("server.js")
+    res.render("index", {text: "world"})
 })
 
 app.listen(3000)
