a6d1ac43698853198dc54424af22e9d6f4aba0c8
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-07-15 21:51:27 +0530

    Fixing bug

--- Express_learn/routes/users.js
+++ Express_learn/routes/users.js
@@ -14,8 +14,12 @@ router.get("/post", (req, res)=>{
     res.send("Post is here")
 })
 
-router.get("/post/:id",(req, res)=>{
-    res.send(`user is here with the Id ${req.prams.id}`)
+router.post("/", (req, res)=>{
+    res.send("create post")
+})
+
+router.get("/:id",(req, res)=>{
+    res.send(`user is here with the Id ${req.params.id}`)
 })
 
 module.exports = router
