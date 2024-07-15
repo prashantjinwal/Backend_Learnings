7c5e425bd57ced5348530abfa3171bff388e0217
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-07-15 18:40:34 +0530

    Add Advance Router

--- Express_learn/routes/users.js
+++ Express_learn/routes/users.js
@@ -10,4 +10,12 @@ router.get("/new", (req, res)=>{
     res.send("New user is here")
 })
 
+router.get("/post", (req, res)=>{
+    res.send("Post is here")
+})
+
+router.get("/post/:id",(req, res)=>{
+    res.send(`user is here with the Id ${req.prams.id}`)
+})
+
 module.exports = router
