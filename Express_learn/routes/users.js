250a86115b08d9efbf40c64f070068be15459254
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-07-15 22:24:04 +0530

    Add Router

--- Express_learn/routes/users.js
+++ Express_learn/routes/users.js
@@ -1,25 +1,37 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("users");
+});
+
+router.get("/new", (req, res) => {
+  res.send("New user is here");
+});
+
+router.get("/post", (req, res) => {
+  res.send("Post is here");
+});
+
+router.post("/", (req, res) => {
+  res.send("create post");
+});
 
 
-router.get("/", (req, res)=>{
-    res.send("users")
-})
+router
+  .route("/:id")
+  .get((req, res) => {
+    res.send(`user is here with the Id ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`Update User With ID ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`Delete User with ID ${req.params.id}`);
+  });
 
-router.get("/new", (req, res)=>{
-    res.send("New user is here")
-})
 
-router.get("/post", (req, res)=>{
-    res.send("Post is here")
-})
 
-router.post("/", (req, res)=>{
-    res.send("create post")
-})
+module.exports = router;
 
-router.get("/:id",(req, res)=>{
-    res.send(`user is here with the Id ${req.params.id}`)
-})
-
-module.exports = router
+// routers search top to bottom... its recommended that /:id  write on bottom
